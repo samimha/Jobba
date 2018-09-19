4fdeae50709168dad9f4e0b74eb83acaef970ca2
Lauri Niemi <niemi.late@gmail.com>
2018-09-19 20:05:42 +0300

    Add material-ui menu and tabs

---
+++ src/components/Tabs.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import {NavLink} from "react-router-dom"
+
+
+// function TabContainer(props) {
+//   return (
+//     <Typography component="div" style={{ padding: 8 * 3 }}>
+//       {props.children}
+//     </Typography>
+//   );
+// }
+
+// TabContainer.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+class SimpleTabs extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs centered value={value} onChange={this.handleChange}>
+          <Tab label="home" className={classes.tabLink}  component={NavLink} to="/"/>
+            <Tab label="new Jobi" className={classes.tabLink}  component={NavLink} to="/create"/>
+            <Tab label="help" className={classes.tabLink}  component={NavLink} to="/help"/>
+          </Tabs>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+SimpleTabs.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTabs);
