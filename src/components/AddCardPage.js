91192d1ed4b6aacc01d98d21c77956fed4e3fa5b
Emily Hernandez <bulle.96@windowslive.com>
2018-10-02 15:32:51 +0300

    Update 'Add card' from #57

--- src/components/AddCardPage.js
+++ src/components/AddCardPage.js
@@ -3,6 +3,24 @@ import { connect } from 'react-redux';
 import CardForm from './CardForm';
 import { startAddCard } from '../actions/cards';
 
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
+const styles = theme => ({
+    paper: {
+        ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
+        marginTop: 14,
+        marginLeft: 10,
+        marginRight: 10,
+        maxHeight: 700,
+        maxWidth: 700
+    }
+});
+
 export class AddCardPage extends React.Component {
     onSubmit = (card) => {
         this.props.startAddCard(card);
@@ -10,12 +28,19 @@ export class AddCardPage extends React.Component {
     };
 
     render() {
+        const { classes } = this.props;
+
         return(
-            <div>
-                <CardForm
-                    onSubmit={this.onSubmit}
-                />
-            </div>
+            <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+            >
+                <Paper className={classes.paper}>
+                    <CardForm onSubmit={this.onSubmit}/>
+                </Paper>
+            </Grid>
         );
     }
 };
@@ -24,4 +49,8 @@ const mapDispatchToProps = (dispatch) => ({
     startAddCard: (card) => dispatch(startAddCard(card))
 });
 
-export default connect(undefined, mapDispatchToProps)(AddCardPage);
+AddCardPage.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default connect(undefined, mapDispatchToProps)(withStyles(styles)(AddCardPage));
